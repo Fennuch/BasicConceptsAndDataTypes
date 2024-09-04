7a2f0e2a09b9850ce2d3e043f30dac5e1277abe8
SkillFacroryCsharpTest <skillfactory.csharp@mail.ru>
2024-09-04 23:38:43 +0200

    Добавьте файлы проекта.

---
+++ BasicConceptsAndDataTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicConceptsAndDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Feona";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
